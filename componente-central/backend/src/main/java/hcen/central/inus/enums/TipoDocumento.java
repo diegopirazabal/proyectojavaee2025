b51c9f4df6a816fca53e10ee8304d641783cdf71
diegopirazabal <diegopirazabal@gmail.com>
2025-10-24 21:20:38 -0300

    fix tipo_documento

--- componente-central/backend/src/main/java/hcen/central/inus/enums/TipoDocumento.java
+++ componente-central/backend/src/main/java/hcen/central/inus/enums/TipoDocumento.java
@@ -3,5 +3,13 @@ package hcen.central.inus.enums;
 public enum TipoDocumento {
     DO,
     PA,
-    OTRO
+    OTRO;
+
+    @Deprecated public static final TipoDocumento CI = DO;
+    @Deprecated public static final TipoDocumento CEDULA = DO;
+    @Deprecated public static final TipoDocumento CEDULA_IDENTIDAD = DO;
+    @Deprecated public static final TipoDocumento PASAPORTE = PA;
+    @Deprecated public static final TipoDocumento PASSPORT = PA;
+    @Deprecated public static final TipoDocumento DNI = OTRO;
+    @Deprecated public static final TipoDocumento OTROS = OTRO;
 }
