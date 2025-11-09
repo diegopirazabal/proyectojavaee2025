30e8d79069912c700bddad8b9ab3ae3bcb6a0187
Diego Pereyra <48370918+diegopirazabal@users.noreply.github.com>
2025-11-08 22:29:00 -0300

    Update startup.sh

--- componente-periferico/backend/startup.sh
+++ componente-periferico/backend/startup.sh
@@ -2,15 +2,6 @@
 
 echo "Configurando datasource en WildFly..."
 
-# Ejecutar script CLI para configurar datasource
-/opt/jboss/wildfly/bin/jboss-cli.sh --file=/opt/jboss/datasource-config.cli
-
-if [ $? -eq 0 ]; then
-    echo "Datasource configurado exitosamente"
-else
-    echo "Error al configurar datasource, continuando con valores por defecto..."
-fi
-
 # Remover welcome-content para que no interfiera con las aplicaciones
 echo "Removiendo welcome-content handler..."
 /opt/jboss/wildfly/bin/jboss-cli.sh --commands="embed-server --server-config=standalone.xml,/subsystem=undertow/server=default-server/host=default-host/location=\/:remove,stop-embedded-server" 2>/dev/null || echo "Welcome-content ya removido o no existe"
