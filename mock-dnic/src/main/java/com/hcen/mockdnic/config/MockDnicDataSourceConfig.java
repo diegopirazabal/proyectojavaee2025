a75a22ca5107e0dc75cf9d251b5816be2085525f
Juampa <juampar1496@gmail.com>
2025-11-10 20:14:40 -0300

    amen 5

--- mock-dnic/src/main/java/com/hcen/mockdnic/config/MockDnicDataSourceConfig.java
+++ mock-dnic/src/main/java/com/hcen/mockdnic/config/MockDnicDataSourceConfig.java
@@ -9,12 +9,12 @@ import jakarta.ejb.Startup;
 @DataSourceDefinition(
         name = "java:app/jdbc/MockDnicDS",
         className = "org.postgresql.xa.PGXADataSource",
-        serverName = "${env.DATABASE_HOST:179.31.2.178}",
+        serverName = "179.31.2.178",
         portNumber = 5432,
-        databaseName = "${env.DATABASE_NAME:postgres}",
-        user = "${env.DATABASE_USER:webadmin}",
-        password = "${env.DATABASE_PASSWORD:V79FHjyEho}"
+        databaseName = "postgres",
+        user = "webadmin",
+        password = "V79FHjyEho"
 )
 public class MockDnicDataSourceConfig {
-    // Datasource con variables de entorno para Railway
+    // Datasource configurado para PostgreSQL externo
 }
